af91eed76473c6f651970f3ae17e72cb83ed0d5f
aditya <adityakanthale@gmail.com>
2021-11-21 17:05:07 -0600

    fetch instance id

--- src/main/scala/Config/LoggerStartupListener.scala
+++ src/main/scala/Config/LoggerStartupListener.scala
@@ -1,5 +1,8 @@
 package Config
 
+import Config.LoggerStartupListener.DEFAULT_INSTANCE_ID
+
+import sys.process.*
 import ch.qos.logback.classic.{Level, Logger, LoggerContext}
 import ch.qos.logback.classic.spi.LoggerContextListener
 import ch.qos.logback.core.spi.ContextAwareBase
@@ -9,6 +12,8 @@ import ch.qos.logback.core.spi.LifeCycle
 object LoggerStartupListener {
   private val INSTANCE_ID_KEY = "EC2_INSTANCE_ID"
   private val DEFAULT_INSTANCE_ID = "i-00000000"
+  private val AWS_METADATA_URL = ""
+  private val INSTANCE_ID_PATH = ""
 }
 
 class LoggerStartupListener extends ContextAwareBase with LoggerContextListener with LifeCycle {
@@ -23,10 +28,19 @@ class LoggerStartupListener extends ContextAwareBase with LoggerContextListener 
   }
 
   def getInstanceId(): String = {
-    System.getenv(LoggerStartupListener.INSTANCE_ID_KEY) match {
-      case x if (x != null && x.length > 0) => x
-      case _ => LoggerStartupListener.DEFAULT_INSTANCE_ID
+
+    try {
+      val token ="curl -X PUT http://169.254.169.254/latest/api/token -H X-aws-ec2-metadata-token-ttl-seconds: 21600".!!
+      val instanceId = s"curl -H X-aws-ec2-metadata-token: $token -v http://169.254.169.254/latest/meta-data/instance-id".!!
+      return instanceId
+    } catch {
+      case e: Exception => DEFAULT_INSTANCE_ID
     }
+
+//    System.getenv(LoggerStartupListener.INSTANCE_ID_KEY) match {
+//      case x if (x != null && x.length > 0) => x
+//      case _ => LoggerStartupListener.DEFAULT_INSTANCE_ID
+//    }
   }
 
   override def stop(): Unit = {
